e8d1678c8445dca4b29f7dc0e15a863875eb3eec
57U <uu57uu@yandex.com>
2024-10-22 11:46:29 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     setWheelNum(wheel_num + 1);
     // 滚轮滑动两次进入新页面
     if (wheel_num > 1) {
-      window.location.href = "http://blog.57u.tech";
+      window.location.assign("http://blog.57u.tech")
       setWheelNum(0);
     }
   })
@@ -19,13 +19,13 @@ function App() {
   return (<div style={{
     ...basicStyle,
     //width: 'auto',
-    padding: '2rem',
+    //padding: '2rem',
   }} >
     <div >
       <Card
-        title={<h3>Welcome to 57U's Page</h3>}
+        title={<h3 className='white'>Welcome to 57U's Page</h3>}
         bordered={false}
-        className='adaptive-element'
+        className='adaptive-element white'
         style={{
           background: '#313638B0',
           top: '20%'
